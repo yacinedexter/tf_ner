ae10753570ccfb4fcb72036a0d5203489afbf19a
yacinedexter <yacine.rsd2@gmail.com>
2019-02-11 14:57:44 -0500

    Update main.py

--- models/chars_lstm_lstm_crf/main.py
+++ models/chars_lstm_lstm_crf/main.py
@@ -12,7 +12,7 @@ import numpy as np
 import tensorflow as tf
 from tf_metrics import precision, recall, f1
 
-DATADIR = '../../data/connl2003'
+DATADIR = '../../data/example'
 
 # Logging
 Path('results').mkdir(exist_ok=True)
