5f13ad2c0444dd16e8c8f5d33fa665664da10998
yacinedexter <yacine.rsd2@gmail.com>
2019-03-14 14:07:44 -0400

    Update metrics.py

--- models/chars_conv_lstm_crf/metrics.py
+++ models/chars_conv_lstm_crf/metrics.py
@@ -5,9 +5,9 @@ __author__ = "Guillaume Genthial"
 import numpy as np
 
 
-train = [99.61, 99.51, 99.29, 99.54, 99.48, 99.47, 99.65, 99.36, 99.46, 99.37]
-testa = [94.25, 94.15, 94.06, 94.28, 94.36, 94.48, 94.21, 94.16, 94.12, 94.17]
-testb = [94.25, 94.15]
+train = [9]
+testa = [9]
+testb = [91.30, 90.53, 90.61, 90.74, 90.88, 90.09, 91.01, 90.90, 90.43, 90.91]
 
 print(np.mean(train), np.std(train), np.max(train), np.min(train))
 print(np.mean(testa), np.std(testa), np.max(testa), np.min(testa))
