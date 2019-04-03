2f91a1e180fd8cd454af697bd96dceac59061b50
yacinedexter <yacine.rsd2@gmail.com>
2019-04-03 16:01:58 -0400

    Update main.py

--- models/char_cnn+lstm_attention/main.py
+++ models/char_cnn+lstm_attention/main.py
@@ -227,7 +227,6 @@ if __name__ == '__main__':
         'buffer': 15000,#buffer_size: A tf.int64 scalar tf.Tensor, representing the number of elements from this dataset from which the new dataset will sample.
         'char_lstm_size': 50,#char lstm unit number (hidden state size)
         'lstm_size': 250,#word lstm unit number (hidden state size)
-        'ATTENTION_SIZE': 50,
         'words': str(Path(DATADIR, 'vocab.words.txt')),
         'chars': str(Path(DATADIR, 'vocab.chars.txt')),
         'tags': str(Path(DATADIR, 'vocab.tags.txt')),
