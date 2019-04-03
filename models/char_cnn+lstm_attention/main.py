f213d8d8d27bf8b2088e5190e80f6ebf0c1f8ff1
yacinedexter <yacine.rsd2@gmail.com>
2019-04-03 15:58:54 -0400

    Update main.py

--- models/char_cnn+lstm_attention/main.py
+++ models/char_cnn+lstm_attention/main.py
@@ -221,7 +221,7 @@ if __name__ == '__main__':
         'dropout': 0.5,
         'num_oov_buckets': 1,#to give index for out of vocabulary
         'epochs': 25,
-        'batch_size': 16,
+        'batch_size': 32,
         'filters': 100,
         'kernel_size': 3,        
         'buffer': 15000,#buffer_size: A tf.int64 scalar tf.Tensor, representing the number of elements from this dataset from which the new dataset will sample.
