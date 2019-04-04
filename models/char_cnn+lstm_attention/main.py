74ec27c362d8c14cd527195ba06cc5c7fa0f4883
yacinedexter <yacine.rsd2@gmail.com>
2019-04-04 11:46:43 -0400

    Update main.py

--- models/char_cnn+lstm_attention/main.py
+++ models/char_cnn+lstm_attention/main.py
@@ -221,11 +221,11 @@ if __name__ == '__main__':
         'dropout': 0.5,
         'num_oov_buckets': 1,#to give index for out of vocabulary
         'epochs': 25,
-        'batch_size': 24,
-        'filters': 100,
+        'batch_size': 32,
+        'filters': 150,
         'kernel_size': 3,        
         'buffer': 15000,#buffer_size: A tf.int64 scalar tf.Tensor, representing the number of elements from this dataset from which the new dataset will sample.
-        'char_lstm_size': 50,#char lstm unit number (hidden state size)
+        'char_lstm_size': 75,#char lstm unit number (hidden state size)
         'lstm_size': 250,#word lstm unit number (hidden state size)
         'words': str(Path(DATADIR, 'vocab.words.txt')),
         'chars': str(Path(DATADIR, 'vocab.chars.txt')),
