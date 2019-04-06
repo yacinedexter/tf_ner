37c86616e8d299551b4ad7593bcfb0c1d6000fde
yacinedexter <yacine.rsd2@gmail.com>
2019-04-06 14:00:05 -0400

    Update main.py

--- models/char_cnn_lstm-LSTM_crf/main.py
+++ models/char_cnn_lstm-LSTM_crf/main.py
@@ -213,12 +213,12 @@ if __name__ == '__main__':
         'dropout': 0.5,
         'num_oov_buckets': 1,#to give index for out of vocabulary
         'epochs': 25,
-        'batch_size': 32,
+        'batch_size': 20,
         'filters': 50,
         'kernel_size': 3,        
         'buffer': 15000,#buffer_size: A tf.int64 scalar tf.Tensor, representing the number of elements from this dataset from which the new dataset will sample.
-        'char_lstm_size': 100,#char lstm unit number (hidden state size)
-        'lstm_size': 200,#word lstm unit number (hidden state size)
+        'char_lstm_size': 50,#char lstm unit number (hidden state size)
+        'lstm_size': 100,#word lstm unit number (hidden state size)
         'words': str(Path(DATADIR, 'vocab.words.txt')),
         'chars': str(Path(DATADIR, 'vocab.chars.txt')),
         'tags': str(Path(DATADIR, 'vocab.tags.txt')),
