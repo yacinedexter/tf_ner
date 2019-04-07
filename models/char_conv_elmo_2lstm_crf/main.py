020de724b1c299a24f54b255e1f242a344431196
yacinedexter <yacine.rsd2@gmail.com>
2019-04-07 07:46:12 -0400

    Update main.py

--- models/char_conv_elmo_2lstm_crf/main.py
+++ models/char_conv_elmo_2lstm_crf/main.py
@@ -207,7 +207,7 @@ if __name__ == '__main__':
         'filters': 50,
         'kernel_size': 3,
         'lstm_size': 100,
-        'lstm2_size': 200,
+        'lstm2_size': 400,
         'words': str(Path(DATADIR, 'vocab.words.txt')),
         'chars': str(Path(DATADIR, 'vocab.chars.txt')),
         'tags': str(Path(DATADIR, 'vocab.tags.txt')),
