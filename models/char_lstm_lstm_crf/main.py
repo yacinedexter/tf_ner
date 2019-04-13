e841a983b1fc21e6487ba13dc9a6530a89f8175c
yacinedexter <yacine.rsd2@gmail.com>
2019-04-13 18:37:29 -0400

    Update main.py

--- models/char_lstm_lstm_crf/main.py
+++ models/char_lstm_lstm_crf/main.py
@@ -135,7 +135,7 @@ def model_fn(features, labels, mode, params):
                 'mask': weights}
     
     word_input = weight_layers(
-        'elmo_input', bilm_ops, l2_coef=1.0, do_layer_norm=True, use_top_only=False)    
+        'word_input', bilm_ops, l2_coef=1.0, do_layer_norm=True, use_top_only=False)    
                                      
     embeddings = tf.layers.dropout(word_input['weighted_op'], rate=dropout, training=training)
     
@@ -167,7 +167,7 @@ def model_fn(features, labels, mode, params):
                 'mask': weights}
     
     weight_sum = weight_layers(
-        'elmo_input', bilm_ops, l2_coef=1.0, do_layer_norm=True, use_top_only=False)    
+        'weight_sum', bilm_ops, l2_coef=1.0, do_layer_norm=True, use_top_only=False)    
                                      
     output = tf.layers.dropout(weight_sum['weighted_op'], rate=dropout, training=training)     
     
