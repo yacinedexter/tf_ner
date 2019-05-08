59711bfadcec9608e4fb5c27d2d1215ef5376e42
yacinedexter <yacine.rsd2@gmail.com>
2019-05-08 14:59:38 -0400

    Update main.py

--- models/wsum_CLSTM_GLSTM_LSTM/main.py
+++ models/wsum_CLSTM_GLSTM_LSTM/main.py
@@ -153,7 +153,7 @@ def model_fn(features, labels, mode, params):
                 'mask': weights}
     
     weight_sum = weight_layers(
-        'elmo_input', bilm_ops, l2_coef=1.0, do_layer_norm=False, use_top_only=False)    
+        'elmo_input', bilm_ops, l2_coef=1.0, do_layer_norm=True, use_top_only=False)    
                                      
     output = tf.layers.dropout(weight_sum['weighted_op'], rate=dropout, training=training)    
     output = tf.layers.dropout(output, rate=dropout, training=training)    
