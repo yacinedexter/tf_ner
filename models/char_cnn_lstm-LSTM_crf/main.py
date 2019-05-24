d5c661df8f748e05986d66a39bba25d4e7587981
yacinedexter <yacine.rsd2@gmail.com>
2019-05-24 15:06:03 -0400

    Update main.py

--- models/char_cnn_lstm-LSTM_crf/main.py
+++ models/char_cnn_lstm-LSTM_crf/main.py
@@ -211,8 +211,8 @@ if __name__ == '__main__':
         'filters': 50,
         'kernel_size': 3,        
         'buffer': 15000,#buffer_size: A tf.int64 scalar tf.Tensor, representing the number of elements from this dataset from which the new dataset will sample.
-        'char_lstm_size': 150,#char lstm unit number (hidden state size)
-        'lstm_size': 200,#word lstm unit number (hidden state size)
+        'char_lstm_size': 100,#char lstm unit number (hidden state size)
+        'lstm_size': 150,#word lstm unit number (hidden state size)
         'words': str(Path(DATADIR, 'vocab.words.txt')),
         'chars': str(Path(DATADIR, 'vocab.chars.txt')),
         'tags': str(Path(DATADIR, 'vocab.tags.txt')),
