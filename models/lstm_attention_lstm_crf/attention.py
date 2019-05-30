c04e0a159acc142c2cd28ada754b89d4ae56b32c
yacinedexter <yacine.rsd2@gmail.com>
2019-05-30 16:08:17 -0400

    Update attention.py

--- models/lstm_attention_lstm_crf/attention.py
+++ models/lstm_attention_lstm_crf/attention.py
@@ -12,17 +12,17 @@ def attention(inputs, attention_size, time_major=False, return_alphas=False):
     # Trainable parameters
     u_omega = tf.Variable(tf.random_normal([attention_size], stddev=0.1))
 
-    with tf.name_scope('v'):
+    with tf.name_scope('u'):
         # Applying fully connected layer with non-linear activation to each of the B*T timestamps;
         #  the shape of `v` is (B,T,D)*(D,A)=(B,T,A), where A=attention_size
-        v = tf.layers.dense(inputs, attention_size, 
+        u = tf.layers.dense(inputs, attention_size, 
                             activation=tf.tanh, 
                             use_bias=True, 
                             bias_initializer=tf.zeros_initializer())
         
-    # For each of the timestamps its vector of size A from `v` is reduced with `u` vector
-    vu = tf.tensordot(v, u_omega, axes=1, name='vu')  # (B,T) shape
-    alphas = tf.nn.softmax(vu, name='alphas')         # (B,T) shape
+    # For each of the timestamps its vector of size A from `u` is reduced with `u_omega` vector
+    uu = tf.tensordot(u, u_omega, axes=1, name='uu')  # (B,T) shape
+    alphas = tf.nn.softmax(uu, name='alphas')         # (B,T) shape
 
     # Output of (Bi-)RNN is reduced with attention vector; the result has (B,D) shape
     output = tf.reduce_sum(inputs * tf.expand_dims(alphas, -1), 1)
