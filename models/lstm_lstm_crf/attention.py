155604a4134d021e2e0f846b97d19eeb220ee8d4
yacinedexter <yacine.rsd2@gmail.com>
2019-05-31 20:25:03 -0400

    Update attention.py

--- models/lstm_lstm_crf/attention.py
+++ models/lstm_lstm_crf/attention.py
@@ -26,7 +26,7 @@ def attention(inputs, attention_size, time_major=False, return_alphas=False):
     uu = tf.tensordot(u, u_omega, axes=1, name='uu')  # (B,T,A)*(A)=(B,T) shape
     alphas = tf.nn.softmax(uu, name='alphas')         # (B,T) shape
 
-    # Output of (Bi-)RNN is reduced with attention vector; the result has (B,T,D) shape
+    # Output of (Bi-)RNN is reduced with attention vector; the result has (B,D) shape
     output = tf.reduce_sum(inputs* tf.expand_dims(alphas, -1), 1)
 
     if not return_alphas:
