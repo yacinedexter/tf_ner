350de429741cfe36b4500e04ed10ae5b43336c86
yacinedexter <yacine.rsd2@gmail.com>
2019-06-04 12:13:55 -0400

    Update attention.py

--- models/lstm_lstm_crf/attention.py
+++ models/lstm_lstm_crf/attention.py
@@ -11,17 +11,15 @@ def attention(inputs, attention_size, time_major=False, return_alphas=False):
 
 
     # Trainable parameters
+    w_omega = tf.Variable(tf.random_normal([hidden_size, attention_size], stddev=0.1))
+    q_omega = tf.Variable(tf.random_normal([hidden_size, attention_size], stddev=0.1)
+    #b_omega = tf.Variable(tf.random_normal([attention_size], stddev=0.1))
     u_omega = tf.Variable(tf.random_normal([attention_size], stddev=0.1))
 
     with tf.name_scope('u'):
         # Applying fully connected layer with non-linear activation to each of the B*T timestamps;
         #  the shape of `v` is (B,T,D)*(D,A)=(B,T,A), where A=attention_size
-        u = tf.layers.dense(inputs, 
-                            attention_size, 
-                            activation=tf.tanh, 
-                            use_bias=True, 
-                            bias_initializer=tf.zeros_initializer())
-        
+        v = tf.tanh(tf.tensordot(inputs, q_omega, axes=1)+tf.tensordot(inputs, w_omega, axes=1))        
     # For each of the timestamps its vector of size A from `u` is reduced with `u_omega` vector
     uu = tf.tensordot(u, u_omega, axes=1, name='uu')  # (B,T,A)*(A)=(B,T) shape
     alphas = tf.nn.softmax(uu, name='alphas')         # (B,T) shape
@@ -33,3 +31,10 @@ def attention(inputs, attention_size, time_major=False, return_alphas=False):
         return output
     else:
     	return output, alphas
+
+                          
+    #    u = tf.layers.dense(inputs, 
+    #                         attention_size, 
+    #                         activation=tf.tanh, 
+    #                         use_bias=True, 
+    #                         bias_initializer=tf.zeros_initializer())
