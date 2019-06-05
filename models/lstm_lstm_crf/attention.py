a3d41751deb7e005326ab2b24f59dd1e420e7e71
yacinedexter <yacine.rsd2@gmail.com>
2019-06-05 14:51:57 -0400

    Update attention.py

--- models/lstm_lstm_crf/attention.py
+++ models/lstm_lstm_crf/attention.py
@@ -16,21 +16,27 @@ def attention(inputs, attention_size, time_major=False, return_alphas=False):
     #b_omega = tf.Variable(tf.random_normal([attention_size], stddev=0.1))
     u_omega = tf.Variable(tf.random_normal([attention_size], stddev=0.1))
 
-    with tf.name_scope('u'):
-        # Applying fully connected layer with non-linear activation to each of the B*T timestamps;
-        #  the shape of `v` is (B,T,D)*(D,A)=(B,T,A), where A=attention_size
-        v = tf.tanh(tf.tensordot(inputs, q_omega, axes=1)+tf.tensordot(inputs, w_omega, axes=1))        
-    # For each of the timestamps its vector of size A from `u` is reduced with `u_omega` vector
-    uu = tf.tensordot(u, u_omega, axes=1, name='uu')  # (B,T,A)*(A)=(B,T) shape
-    alphas = tf.nn.softmax(uu, name='alphas')         # (B,T) shape
+    
+    # Applying fully connected layer with non-linear activation to each of the B*T timestamps;
+    #  the shape of `v` is (B,T,D)*(D,A)=(B,T,A), where A=attention_size
+    outputs=[]
+    s_inputs=tf.unstack(inputs, axis=1)
+    for s_i in s_inputs:
+        u = tf.tanh(tf.tensordot(tf.expand_dims(s_i, -2), q_omega, axes=1)+tf.tensordot(inputs, w_omega, axes=1))        
+        # For each of the timestamps its vector of size A from `u` is reduced with `u_omega` vector
+        uu = tf.tensordot(u, u_omega, axes=1, name='uu')  # (B,T,A)*(A)=(B,T) shape
+        alphas = tf.nn.softmax(uu, name='alphas')         # (B,T) shape
 
-    # Output of (Bi-)RNN is reduced with attention vector; the result has (B,D) shape
-    output = tf.reduce_sum(inputs* tf.expand_dims(alphas, -1), 1)
+        # Output of (Bi-)RNN is reduced with attention vector; the result has (B,D) shape
+        output = tf.reduce_sum(inputs* tf.expand_dims(alphas, -1), 1)
+        outputs.append(output)
+    
+    outputs=tf.stack(outputs, axis=1)
 
     if not return_alphas:
-        return output
+        return outputs
     else:
-    	return output, alphas
+    	return outputs, alphas
 
                           
     #    u = tf.layers.dense(inputs, 
