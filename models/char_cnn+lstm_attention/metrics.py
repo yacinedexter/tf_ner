360af2d45040716f8130f6e9f71ef904b9d225a7
yacinedexter <yacine.rsd2@gmail.com>
2019-11-16 15:42:14 -0500

    Update metrics.py

--- models/char_cnn+lstm_attention/metrics.py
+++ models/char_cnn+lstm_attention/metrics.py
@@ -5,9 +5,9 @@ __author__ = "Guillaume Genthial"
 import numpy as np
 
 
-train = [99.12, 98.71, 98.66, 98.25, 98.68, 98.49, 99.07, 98.25, 98.63, 98.28]
-testa = [94.33, 94.41, 94.11, 93.88, 94.07, 93.84, 94.22, 94.12, 94.36, 93.94]
-testb = [90.89, 91.06, 90.23, 90.60, 91.06, 90.71, 90.91, 90.46, 90.94, 90.99]
+train = [0]
+testa = [0]
+testb = [90.47, 90.46, 90.40, 90.34, 90.62]
 
 print(np.mean(train), np.std(train))
 print(np.mean(testa), np.std(testa))
