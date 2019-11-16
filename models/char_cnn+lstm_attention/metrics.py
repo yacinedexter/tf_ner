e016ce07ed08703ab4ba0e48ade992098036a3e3
yacinedexter <yacine.rsd2@gmail.com>
2019-11-16 16:02:16 -0500

    Update metrics.py

--- models/char_cnn+lstm_attention/metrics.py
+++ models/char_cnn+lstm_attention/metrics.py
@@ -7,7 +7,7 @@ import numpy as np
 
 train = [0]
 testa = [0]
-testb = [90.47, 90.46, 90.40, 90.34, 90.62]
+testb = [85.39, 85.17, 85.19, 85.05, 85.37]
 
 print(np.mean(train), np.std(train))
 print(np.mean(testa), np.std(testa))
