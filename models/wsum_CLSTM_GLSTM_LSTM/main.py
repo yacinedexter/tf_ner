47e40739a56a59f30cca24cca89c295f78669a75
yacinedexter <yacine.rsd2@gmail.com>
2019-11-23 01:07:51 -0500

    Update main.py

--- models/wsum_CLSTM_GLSTM_LSTM/main.py
+++ models/wsum_CLSTM_GLSTM_LSTM/main.py
@@ -207,7 +207,7 @@ def model_fn(features, labels, mode, params):
                 mode, loss=loss, eval_metric_ops=metrics)
 
         elif mode == tf.estimator.ModeKeys.TRAIN:
-            train_op = tf.train.ProximalAdagradOptimizer(learning_rate=0.001).minimize(
+            train_op = tf.train.ProximalGradientDescentOptimizer(learning_rate=0.001).minimize(
                 loss, global_step=tf.train.get_or_create_global_step())
             return tf.estimator.EstimatorSpec(
                 mode, loss=loss, train_op=train_op)
@@ -220,7 +220,7 @@ if __name__ == '__main__':
         'dim_chars': 100,
         'dropout': 0.5,
         'num_oov_buckets': 1,
-        'epochs': 25,
+        'epochs': 50,
         'batch_size': 32,
         'buffer': 15000,
         'char_lstm_size': 150,
