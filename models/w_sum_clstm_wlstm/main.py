a3d20d868b11b9b02dbff2c3f7e39e28e25f87b2
yacinedexter <yacine.rsd2@gmail.com>
2019-11-22 23:56:57 -0500

    Update main.py

--- models/w_sum_clstm_wlstm/main.py
+++ models/w_sum_clstm_wlstm/main.py
@@ -196,7 +196,7 @@ def model_fn(features, labels, mode, params):
                 mode, loss=loss, eval_metric_ops=metrics)
 
         elif mode == tf.estimator.ModeKeys.TRAIN:
-            train_op = tf.train.ProximalAdagradOptimizer(learning_rate=0.001).minimize(
+            train_op = tf.train.ProximalGradientDescentOptimizer(learning_rate=0.001).minimize(
                 loss, global_step=tf.train.get_or_create_global_step())
             return tf.estimator.EstimatorSpec(
                 mode, loss=loss, train_op=train_op)
