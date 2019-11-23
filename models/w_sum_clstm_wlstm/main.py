caadf0d425cf06d4e12d62d04ae6f4974d357e13
yacinedexter <yacine.rsd2@gmail.com>
2019-11-23 09:53:45 -0500

    Update main.py

--- models/w_sum_clstm_wlstm/main.py
+++ models/w_sum_clstm_wlstm/main.py
@@ -209,7 +209,7 @@ if __name__ == '__main__':
         'dim_chars': 100,
         'dropout': 0.5,
         'num_oov_buckets': 1,
-        'epochs': 25,
+        'epochs': 50,
         'batch_size': 32,
         'buffer': 15000,
         'char_lstm_size': 200,
